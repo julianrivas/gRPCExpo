9aa1a2b48017d1050ceb3e12475f61b50f5d9598
Julian Rivas <juliandarivas@hotmail.com>
2025-04-29 01:55:24 -0500

    add benchmark esporter configuration

--- sources/Performance/Benchmark/Benchmark/Program.cs
+++ sources/Performance/Benchmark/Benchmark/Program.cs
@@ -1,5 +1,12 @@
 ﻿using Benchmark;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Running;
 
-//BenchmarkRunner.Run<ItemsMessagingBenchmark>();
-BenchmarkRunner.Run<BitacorasMessagingBenchmark>();
+var config = ManualConfig.Create(DefaultConfig.Instance)
+    .AddExporter(HtmlExporter.Default)
+    .AddDiagnoser(MemoryDiagnoser.Default); 
+
+BenchmarkRunner.Run<ItemsMessagingBenchmark>(config);
+BenchmarkRunner.Run<BitacorasMessagingBenchmark>(config);
